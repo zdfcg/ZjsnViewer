89dbafb20803ac41a23e1d80331252edc9ca379d
William Lyu <paleneutron@outlook.com>
2016-06-17 19:46:19 +0800

    修改错别字……

--- app/src/main/java/me/crafter/android/zjsnviewer/config/WebSite.java
+++ app/src/main/java/me/crafter/android/zjsnviewer/config/WebSite.java
@@ -22,7 +22,7 @@ public class WebSite {
 
     private WebSite(){
 
-        addNameAndUrl("幻梦官网(没啥卯月)", "http://www.jianniang.com/default.html");
+        addNameAndUrl("幻萌官网(没啥卯月)", "http://www.jianniang.com/default.html");
         addNameAndUrl("舰少资料库", "http://js.ntwikis.com/" , "http://js.ntwikis.com/jsp/apps/cancollezh/img/can.ico");
         addNameAndUrl("舰R百科", "http://www.zjsnrwiki.com/wiki/%E9%A6%96%E9%A1%B5", "http://www.zjsnrwiki.com/icon.ico");
         addNameAndUrl("萌娘百科", "https://zh.moegirl.org/%E6%88%98%E8%88%B0%E5%B0%91%E5%A5%B3","https://zh.moegirl.org/favicon.ico");
