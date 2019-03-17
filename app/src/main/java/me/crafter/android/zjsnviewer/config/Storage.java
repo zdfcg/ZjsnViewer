790cb2300c6892c6e4e02333437b6f3b577cd2fd
zdfcg <2210450389@qq.com>
2019-03-17 14:37:36 +0800

    Update Storage.java

--- app/src/main/java/me/crafter/android/zjsnviewer/config/Storage.java
+++ app/src/main/java/me/crafter/android/zjsnviewer/config/Storage.java
@@ -134,5 +134,46 @@ public class Storage {
     public static boolean root(Context context){
         return PreferenceManager.getDefaultSharedPreferences(context).getBoolean("root", false);
     }
-
+    public static String getTopAppName(Context context) {
+    ActivityManager mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    String strName = "";
+    try {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            strName = getLollipopFGAppPackageName(context);
+        } else {
+            strName = mActivityManager.getRunningTasks(1).get(0).topActivity.getClassName();
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return strName;
+    }
+    
+    private static String getLollipopFGAppPackageName(Context ctx) {
+    try {
+        UsageStatsManager usageStatsManager = (UsageStatsManager) ctx.getSystemService("usagestats");
+        long milliSecs = 60 * 1000;
+        Date date = new Date();
+        List<UsageStats> queryUsageStats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, date.getTime() - milliSecs, date.getTime());
+        if (queryUsageStats.size() > 0) {
+            Log.i("LPU", "queryUsageStats size: " + queryUsageStats.size());
+        }
+        long recentTime = 0;
+        String recentPkg = "";
+        for (int i = 0; i < queryUsageStats.size(); i++) {
+            UsageStats stats = queryUsageStats.get(i);
+            if (i == 0 && !"com.huanmeng.zhanjian2".equals(stats.getPackageName())) {
+                Log.i("LPU", "PackageName: " + stats.getPackageName() + " " + stats.getLastTimeStamp());
+            }
+            if (stats.getLastTimeStamp() > recentTime) {
+                recentTime = stats.getLastTimeStamp();
+                recentPkg = stats.getPackageName();
+            }
+        }
+        return recentPkg;
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return "";
+    }
 }
